d71013f679034189cfe240c9b45f895a506068b7
ht-oliver <holiver327@gmail.com>
2025-08-28 11:17:29 -0700

    made quarto doc

---
+++ R/apply_rollmean.R
@@ -0,0 +1,10 @@
+
+apply_rollavg <- function(df, date_col, constituent) {
+  sapply(df[[date_col]], FUN = roll_avg,
+         focal_date = 
+    df[[date_col]], #indexing into the dataframe
+    dates = df[[date_col]],
+    conc = df[[constituent]],
+    win_size_wks = 9
+  )
+}
--- R/roll_mean.R
+++ R/roll_mean.R
@@ -1,5 +1,5 @@
 roll_avg <- function(focal_date, dates, concs, win_size_wks) {
-  # Which dates are in the window. Converting wks to days
+  # Determine which dates are in the window. Convert weeks to days
   is_in_window <- (dates > ((focal_date) - (win_size_wks / 2) * 7)) &
     (dates < (focal_date + (win_size_wks / 2) * 7))
   # Find the associated concentrations
@@ -10,15 +10,6 @@ roll_avg <- function(focal_date, dates, concs, win_size_wks) {
   return(result)
 }
 
-apply_rollmean <- function(df, date_col, constituent) {
-  sapply(
-    df$date_col,
-    roll_avg,
-    dates = df$date_col,
-    conc = df$constituent,
-    win_size_wks = 9
-  )
-}
 
 
   
