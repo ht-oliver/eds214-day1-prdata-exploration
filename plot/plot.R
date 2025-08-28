ad4785de7bf6918585f305da9de3d731a35b1e0d
ht-oliver <holiver327@gmail.com>
2025-08-28 13:36:48 -0700

    prepared for feedback

--- plot/plot.R
+++ plot/plot.R
@@ -1,10 +1,9 @@
 # This script contains the plot that produces Fig 3
 
-source("analysis/analysis.R")
+#source("analysis/analysis.R")
 
-ggplot(data_comb_avgs, x = sample_date) +
+Fig_3 <- ggplot(data_comb_avgs, x = sample_date) +
   geom_line(aes( x = sample_date, y = ca_avg)) +
-  geom_line(aes(x = sample_date, y = k_avg)) +
-  geom_line(aes(x = sample_date, y = mg_avg)) +
-  geom_line(aes(x = sample_date, y = no3_n_avg)) +
-  geom_line(aes(x = sample_date, y = nh4_n_avg))
+  geom_line(aes(x = sample_date, y = k_avg))
+
+Fig_3
