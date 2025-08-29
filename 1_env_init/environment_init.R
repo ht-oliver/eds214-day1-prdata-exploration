a74efd578980a81934af98841015c146ce6f93f9
ht-oliver <holiver327@gmail.com>
2025-08-29 11:37:57 -0700

    activated package installs

--- 1_env_init/environment_init.R
+++ 1_env_init/environment_init.R
@@ -1,6 +1,6 @@
 
 # This document installs the packages necessary to perform analysis
 
-# install.packages(c("tidyverse", "here", "janitor", "lubridate"))
+install.packages(c("tidyverse", "here", "janitor", "lubridate"))
 
 
